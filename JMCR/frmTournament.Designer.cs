ef0eac699a5a6c1a84d3edb7f01eae261d7f1650
akio21r <akio21r@gmail.com>
2019-11-24 14:19:16 +0900

    データ構造を大幅変更する前

--- JMCR/frmTournament.Designer.cs
+++ JMCR/frmTournament.Designer.cs
@@ -42,19 +42,17 @@
 			this.lblCarRight = new System.Windows.Forms.Label();
 			this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
 			this.button1 = new System.Windows.Forms.Button();
-			this.lstNo = new System.Windows.Forms.ListBox();
+			this.lstDataMeibo = new System.Windows.Forms.ListBox();
 			this.dataGridView1 = new System.Windows.Forms.DataGridView();
-			this.no = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.school = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.car = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.lstSchool = new System.Windows.Forms.ListBox();
-			this.lstName = new System.Windows.Forms.ListBox();
-			this.lstCar = new System.Windows.Forms.ListBox();
+			this.dataGridView2 = new System.Windows.Forms.DataGridView();
+			this.lstDataPair = new System.Windows.Forms.ListBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			((System.ComponentModel.ISupportInitialize)(this.pctLeft)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pctRight)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// pctLeft
@@ -113,11 +111,11 @@
 			// lblSchoolLeft
 			// 
 			this.lblSchoolLeft.AutoSize = true;
-			this.lblSchoolLeft.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.lblSchoolLeft.Font = new System.Drawing.Font("HG正楷書体-PRO", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
 			this.lblSchoolLeft.ForeColor = System.Drawing.Color.Yellow;
 			this.lblSchoolLeft.Location = new System.Drawing.Point(13, 433);
 			this.lblSchoolLeft.Name = "lblSchoolLeft";
-			this.lblSchoolLeft.Size = new System.Drawing.Size(82, 24);
+			this.lblSchoolLeft.Size = new System.Drawing.Size(85, 24);
 			this.lblSchoolLeft.TabIndex = 4;
 			this.lblSchoolLeft.Text = "高校名";
 			// 
@@ -135,11 +133,11 @@
 			// lblNameLeft
 			// 
 			this.lblNameLeft.AutoSize = true;
-			this.lblNameLeft.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.lblNameLeft.Font = new System.Drawing.Font("HG正楷書体-PRO", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(128)));
 			this.lblNameLeft.ForeColor = System.Drawing.Color.Yellow;
 			this.lblNameLeft.Location = new System.Drawing.Point(13, 470);
 			this.lblNameLeft.Name = "lblNameLeft";
-			this.lblNameLeft.Size = new System.Drawing.Size(58, 24);
+			this.lblNameLeft.Size = new System.Drawing.Size(60, 24);
 			this.lblNameLeft.TabIndex = 4;
 			this.lblNameLeft.Text = "氏名";
 			// 
@@ -157,11 +155,12 @@
 			// lblCarLeft
 			// 
 			this.lblCarLeft.AutoSize = true;
-			this.lblCarLeft.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.lblCarLeft.BackColor = System.Drawing.Color.Transparent;
+			this.lblCarLeft.Font = new System.Drawing.Font("HG正楷書体-PRO", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
 			this.lblCarLeft.ForeColor = System.Drawing.Color.Yellow;
 			this.lblCarLeft.Location = new System.Drawing.Point(13, 512);
 			this.lblCarLeft.Name = "lblCarLeft";
-			this.lblCarLeft.Size = new System.Drawing.Size(107, 24);
+			this.lblCarLeft.Size = new System.Drawing.Size(180, 33);
 			this.lblCarLeft.TabIndex = 4;
 			this.lblCarLeft.Text = "カーネーム";
 			// 
@@ -184,6 +183,7 @@
 			this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
 			this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(297, 178);
 			this.axWindowsMediaPlayer1.TabIndex = 5;
+			this.axWindowsMediaPlayer1.Visible = false;
 			// 
 			// button1
 			// 
@@ -195,75 +195,63 @@
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
-			// lstNo
+			// lstDataMeibo
 			// 
-			this.lstNo.FormattingEnabled = true;
-			this.lstNo.ItemHeight = 12;
-			this.lstNo.Location = new System.Drawing.Point(440, 89);
-			this.lstNo.Name = "lstNo";
-			this.lstNo.Size = new System.Drawing.Size(44, 124);
-			this.lstNo.TabIndex = 7;
+			this.lstDataMeibo.FormattingEnabled = true;
+			this.lstDataMeibo.ItemHeight = 12;
+			this.lstDataMeibo.Location = new System.Drawing.Point(440, 89);
+			this.lstDataMeibo.Name = "lstDataMeibo";
+			this.lstDataMeibo.Size = new System.Drawing.Size(415, 124);
+			this.lstDataMeibo.TabIndex = 7;
+			this.lstDataMeibo.SelectedIndexChanged += new System.EventHandler(this.lstData_SelectedIndexChanged);
 			// 
 			// dataGridView1
 			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.AllowUserToResizeRows = false;
 			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.no,
-            this.school,
-            this.name,
-            this.car});
-			this.dataGridView1.Location = new System.Drawing.Point(748, 374);
+			this.dataGridView1.Location = new System.Drawing.Point(565, 226);
 			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.RowHeadersVisible = false;
 			this.dataGridView1.RowTemplate.Height = 21;
-			this.dataGridView1.Size = new System.Drawing.Size(288, 150);
-			this.dataGridView1.TabIndex = 8;
+			this.dataGridView1.ShowEditingIcon = false;
+			this.dataGridView1.Size = new System.Drawing.Size(471, 150);
+			this.dataGridView1.TabIndex = 12;
+			this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+			this.dataGridView1.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowEnter);
+			this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
 			// 
-			// no
+			// dataGridView2
 			// 
-			this.no.HeaderText = "No";
-			this.no.Name = "no";
+			this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView2.Location = new System.Drawing.Point(413, 288);
+			this.dataGridView2.Name = "dataGridView2";
+			this.dataGridView2.RowTemplate.Height = 21;
+			this.dataGridView2.Size = new System.Drawing.Size(138, 150);
+			this.dataGridView2.TabIndex = 13;
 			// 
-			// school
+			// lstDataPair
 			// 
-			this.school.HeaderText = "School";
-			this.school.Name = "school";
+			this.lstDataPair.FormattingEnabled = true;
+			this.lstDataPair.ItemHeight = 12;
+			this.lstDataPair.Location = new System.Drawing.Point(281, 19);
+			this.lstDataPair.Name = "lstDataPair";
+			this.lstDataPair.Size = new System.Drawing.Size(120, 88);
+			this.lstDataPair.TabIndex = 14;
+			this.lstDataPair.SelectedIndexChanged += new System.EventHandler(this.lstDataPair_SelectedIndexChanged);
 			// 
-			// name
+			// pictureBox1
 			// 
-			this.name.HeaderText = "Name";
-			this.name.Name = "name";
-			// 
-			// car
-			// 
-			this.car.HeaderText = "Car";
-			this.car.Name = "car";
-			// 
-			// lstSchool
-			// 
-			this.lstSchool.FormattingEnabled = true;
-			this.lstSchool.ItemHeight = 12;
-			this.lstSchool.Location = new System.Drawing.Point(490, 89);
-			this.lstSchool.Name = "lstSchool";
-			this.lstSchool.Size = new System.Drawing.Size(49, 88);
-			this.lstSchool.TabIndex = 9;
-			// 
-			// lstName
-			// 
-			this.lstName.FormattingEnabled = true;
-			this.lstName.ItemHeight = 12;
-			this.lstName.Location = new System.Drawing.Point(550, 87);
-			this.lstName.Name = "lstName";
-			this.lstName.Size = new System.Drawing.Size(120, 88);
-			this.lstName.TabIndex = 10;
-			// 
-			// lstCar
-			// 
-			this.lstCar.FormattingEnabled = true;
-			this.lstCar.ItemHeight = 12;
-			this.lstCar.Location = new System.Drawing.Point(582, 199);
-			this.lstCar.Name = "lstCar";
-			this.lstCar.Size = new System.Drawing.Size(120, 88);
-			this.lstCar.TabIndex = 11;
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.Image3;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(1048, 554);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 15;
+			this.pictureBox1.TabStop = false;
 			// 
 			// frmTournament
 			// 
@@ -271,11 +259,10 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.MidnightBlue;
 			this.ClientSize = new System.Drawing.Size(1048, 554);
-			this.Controls.Add(this.lstCar);
-			this.Controls.Add(this.lstName);
-			this.Controls.Add(this.lstSchool);
+			this.Controls.Add(this.lstDataPair);
+			this.Controls.Add(this.dataGridView2);
 			this.Controls.Add(this.dataGridView1);
-			this.Controls.Add(this.lstNo);
+			this.Controls.Add(this.lstDataMeibo);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.lblCarRight);
 			this.Controls.Add(this.lblNameRight);
@@ -289,14 +276,19 @@
 			this.Controls.Add(this.txtLeft);
 			this.Controls.Add(this.pctLeft);
 			this.Controls.Add(this.axWindowsMediaPlayer1);
+			this.Controls.Add(this.pictureBox1);
+			this.KeyPreview = true;
 			this.Name = "frmTournament";
 			this.Text = "決勝トーナメント";
 			this.Load += new System.EventHandler(this.frmTournament_Load);
+			this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmTournament_KeyPress);
 			this.Resize += new System.EventHandler(this.frmTournament_Resize);
 			((System.ComponentModel.ISupportInitialize)(this.pctLeft)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pctRight)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -317,15 +309,11 @@
 		private System.Windows.Forms.Label lblCarRight;
 		private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
 		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.ListBox lstNo;
+		private System.Windows.Forms.ListBox lstDataMeibo;
 		private System.Windows.Forms.DataGridView dataGridView1;
-		private System.Windows.Forms.DataGridViewTextBoxColumn no;
-		private System.Windows.Forms.DataGridViewTextBoxColumn school;
-		private System.Windows.Forms.DataGridViewTextBoxColumn name;
-		private System.Windows.Forms.DataGridViewTextBoxColumn car;
-		private System.Windows.Forms.ListBox lstSchool;
-		private System.Windows.Forms.ListBox lstName;
-		private System.Windows.Forms.ListBox lstCar;
+		private System.Windows.Forms.DataGridView dataGridView2;
+		private System.Windows.Forms.ListBox lstDataPair;
+		private System.Windows.Forms.PictureBox pictureBox1;
 	}
 }
 
--- JMCR/frmTournament.cs
+++ JMCR/frmTournament.cs
@@ -11,6 +11,10 @@ namespace WindowsFormsApplication1
 {
 	public partial class frmTournament : Form
 	{
+		DataTable table				= new DataTable("Table");
+		String[,] strDataMeibo		= new String[1000, 4];	//基本データ
+		String[,] strDataPair		= new String[1000, 2];	//対戦表
+
 		int		MarginWOut			= 20;
 		int		MarginWIn			= 100;
 		int		MarginTop			= 20;
@@ -33,29 +37,61 @@ namespace WindowsFormsApplication1
 			axWindowsMediaPlayer1.URL = @"素材\fire.mp4";	//渡されたファイルURLを読み込み
 
 			ResizeComponents();
-			CSVFileLoad();
+			CSVFileLoad_Meibo();
+			CSVFileLoad_Pair();
 		}
 
-		private void CSVFileLoad()
+		private void CSVFileLoad_Meibo()
 		{
+            // カラム名の追加
+            table.Columns.Add("No");
+            table.Columns.Add("学校");
+            table.Columns.Add("氏名");
+            table.Columns.Add("カーネーム");
+	
+			// CSVファイルの読み込み
 			string line;
 			string[] field;
 			System.IO.StreamReader reader = new System.IO.StreamReader(@"データ\data.csv", Encoding.Default);
-			lstNo.Items.Clear();
-			while(!reader.EndOfStream){
+			lstDataMeibo.Items.Clear();
+			for(int n=0; !reader.EndOfStream; n++){
 				line = reader.ReadLine();
 				field = line.Split(',');
-				lstNo.Items.Add(field[0]);
-				lstSchool.Items.Add(field[1]);
-				lstName.Items.Add(field[2]);
-				lstCar.Items.Add(field[3]);
+				lstDataMeibo.Items.Add(field[0] + '\t' + field[1] + '\t' + field[2] + '\t' + field[3]);
+
+				// データを追加
+				table.Rows.Add(field[0], field[1], field[2], field[3]);
+	
+				// String[,]
+				strDataMeibo[n, 0] = field[0];
+				strDataMeibo[n, 1] = field[1];
+				strDataMeibo[n, 2] = field[2];
+				strDataMeibo[n, 3] = field[3];
 			}
 			reader.Close();
+		
+	
+		
+            dataGridView1.DataSource = table;
 		}
 
-		private void button1_Click(object sender, EventArgs e)
+		private void CSVFileLoad_Pair()
 		{
- 
+			// CSVファイルの読み込み
+			string line;
+			string[] field;
+			System.IO.StreamReader reader = new System.IO.StreamReader(@"データ\dataPair.csv", Encoding.Default);
+			lstDataPair.Items.Clear();
+			for(int n=0; !reader.EndOfStream; n++){
+				line = reader.ReadLine();
+				field = line.Split(',');
+				lstDataPair.Items.Add(field[0] + '\t' + field[1]);
+
+				// String[,]
+				strDataPair[n, 0] = field[0];
+				strDataPair[n, 1] = field[1];
+			}
+			reader.Close();
 		}
 
 		private void ResizeComponents()
@@ -64,6 +100,11 @@ namespace WindowsFormsApplication1
 			int TextHeight		= TextNameHeightPer * ClientSize.Height / 100;
 			int TextTop			= ClientSize.Height - MarginBottom - TextHeight + TextMargin;
 
+			int FontHeight		= ClientSize.Height / 20;
+			Font fnt			= new Font("HG正楷書体-PRO", FontHeight);
+			lblSchoolLeft.Font	= lblSchoolRight.Font = fnt;
+			lblNameLeft.Font	= lblNameRight.Font = fnt;
+			lblCarLeft.Font		= lblCarRight.Font = fnt;
 
 			//lblCar
 			lblCarLeft.Left		= MarginWOut;
@@ -115,12 +156,6 @@ namespace WindowsFormsApplication1
 		private void txtLeft_TextChanged(object sender, EventArgs e)
 		{
 			pctLeft.ImageLocation = @"データ\" + txtLeft.Text + ".jpg";
-	//		for(int i=0; i<lstNo.Items.Count; i++){
-	//			if(lstNo.Items[i].ToString == txtLeft.Text){
-	//				lblSchoolLeft.Text = lstSchool.Items[i].ToString;
-	//			}
-	//		}
-
 		}
 
 		private void txtRight_TextChanged(object sender, EventArgs e)
@@ -128,6 +163,61 @@ namespace WindowsFormsApplication1
 			pctRight.ImageLocation = @"データ\" + txtRight.Text + ".jpg";
 		}
 
+		private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+		{
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
+
+		private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+		{
+			txtLeft.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+			lblSchoolLeft.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+			lblNameLeft.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+			lblCarLeft.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+
+		}
+
+		private void lstData_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			int n = lstDataMeibo.SelectedIndex;
+			txtLeft.Text = strDataMeibo[n, 0];
+			lblSchoolLeft.Text = strDataMeibo[n, 1];
+			lblNameLeft.Text = strDataMeibo[n, 2];
+			lblCarLeft.Text = strDataMeibo[n, 3];
+
+		}
+
+		private void frmTournament_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			switch(e.KeyChar){
+				case 'd':
+					dataGridView1.Visible = !dataGridView1.Visible;
+					if(dataGridView1.Visible){
+						dataGridView1.Focus();
+					}
+
+					break;
+			}
+		}
+
+		private void lstDataPair_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			int n = lstDataPair.SelectedIndex;
+		//	SelectLeft (strDataPair[lstDataPair.SelectedIndex, 0]);
+		//	SelectRight(strDataPair[lstDataPair.SelectedIndex, 1]);
+		}
+
+		private void SelectLeft(int n)
+		{
+		}
 
 	}
 }
